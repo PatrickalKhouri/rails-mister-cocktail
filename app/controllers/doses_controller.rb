59c6e192608e3bf8a561013ab9253111a6cb3984
Patrick Khouri <patrick.darya@gmail.com>
2020-08-27 16:33:15 -0300

    almost done with the back end. needs to show doses in cocktail and destroy doses

---
+++ app/controllers/doses_controller.rb
@@ -0,0 +1,27 @@
+class DosesController < ApplicationController
+  def new
+    @dose = Dose.new
+    @cocktail = Cocktail.find(params[:cocktail_id])
+  end
+
+  def create
+    @dose = Dose.new(dose_params)
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    if @dose.save
+      redirect_to @dose
+    else  
+      render :new
+    end
+  end
+
+  def destroy
+    @dose = Dose.find(params[:id])
+    @dose.destroy
+  end
+
+  private
+
+  def dose_params
+    params.require(:dose).permit(:description, :cocktail_id, :ingredient_id)
+  end
+end
